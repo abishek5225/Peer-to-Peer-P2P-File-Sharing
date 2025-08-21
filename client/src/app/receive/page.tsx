0a56d5fd1f53f0a00648ae6e81f69e92b60198a3
abishek5225 <abishekchaulagain5225@gmail.com>
2025-08-21 16:34:07 +0545

    updated

--- client/src/app/receive/page.tsx
+++ client/src/app/receive/page.tsx
@@ -8,7 +8,18 @@ export default function ReceivePage(){
         setDownloading(true);
         try{
             const res= await fetch("http://localhost:8080/receive");
-            
-        }
+            const blob= await res.blob();
+
+        } catch (ex) {
+        console.log(ex);
     }
+    }
+    return(
+        <div className="bg-gray-500 min-h-screen flex flex-col items-center justify-center">
+            <h1 className="text-5xl font-bold mb-4">Receive a File</h1>
+            <div className="h-24 ">
+
+            </div>
+        </div>
+    )
 }
